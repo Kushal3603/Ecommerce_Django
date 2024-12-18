8a4d01b17e2bc5bb38f056002e2726d10c291574
Kushal Shah <kushalshah1604@gmail.com>
2024-12-18 12:45:58 +0530

    Added functionality of update profile and password

--- cart/cart.py
+++ cart/cart.py
@@ -10,13 +10,14 @@ class Cart():
 
         self.cart=cart
 
-    def add(self, product):
+    def add(self, product, quantity):
         product_id=str(product.id)
-
+        product_qty=str(quantity)
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id]={'price':str(product.price)}
+            # self.cart[product_id]={'price':str(product.price)}
+            self.cart[product_id]=int(product_qty)
 
         self.session.modified=True
 
@@ -27,3 +28,49 @@ class Cart():
         product_ids=self.cart.keys()
         products=Product.objects.filter(id__in=product_ids)
         return products
+    
+    def get_quants(self):
+        quantities=self.cart
+        return quantities
+    
+    def update(self,product_id,quantity):
+        
+        product_qty=int(quantity)
+        product_id=str(product_id)
+
+        ourcart=self.cart
+        ourcart[product_id]=product_qty
+
+        self.session.modified=True
+        thing=self.cart
+        return thing
+
+    def delete(self,product):
+        product_id=str(product)
+
+        if product_id in self.cart:
+            del self.cart[product_id]
+
+        self.session.modified=True
+
+    def cart_total(self):
+        productids=self.cart.keys()
+        products=Product.objects.filter(id__in=productids)
+
+        quantities=self.cart
+        total=0
+
+        for key,value in quantities.items():
+            key=int(key)
+            for product in products:
+                if product.id==key:
+                    if product.is_sale:
+                        total=total+(product.sale_price*value) 
+                    else:
+                        total=total+(product.price*value) 
+        return total 
+
+
+            
+
+   
--- cart/views.py
+++ cart/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
 
 def cart_summary(request):
     cart=Cart(request)
     cart_products=cart.get_prods()
-    return render(request,'cart_summary.html',{"cart_products":cart_products})
+    quantities=cart.get_quants()
+    totals=cart.cart_total()
+    return render(request,'cart_summary.html',{"cart_products":cart_products,"quantities":quantities,"totals":totals})
 
 def cart_add(request):
     cart=Cart(request)
@@ -14,15 +18,38 @@ def cart_add(request):
         product_id=int(request.POST.get('product_id'))
         product_qty=int(request.POST.get('product_qty'))
         product=get_object_or_404(Product, id=product_id)
-        cart.add(product=product)
+        cart.add(product=product,quantity=product_qty)
         cart_quantity=cart.__len__()
         response=JsonResponse({'qty':cart_quantity})
+        messages.success(request,("Product Added to Cart."))
         return response
         # response=JsonResponse({'Product Name: ':product.name})
         # return response
 
 def cart_update(request):
-    pass
+    if request.method == 'POST' and request.POST.get('action') == 'post':
+        # Fetch product ID and quantity from POST data
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+
+        # Update the cart using the Cart class
+        cart = Cart(request)
+        cart.update(product_id=product_id, quantity=product_qty)
+
+        # Get the updated cart quantity
+        cart_quantity = cart.__len__()
+        # Return the updated cart quantity as a JSON response
+        response= JsonResponse({'qty': cart_quantity})
+        messages.success(request,("Your Cart Has Been Updated."))
+        return response
+
+    return JsonResponse({'error': 'Invalid request'}, status=400)
 
 def cart_delete(request):
-    pass
+    cart=Cart(request)
+    if request.POST.get('action')=='post':
+        product_id=int(request.POST.get('product_id'))
+        cart.delete(product=product_id)
+        response=JsonResponse({'product':product_id})
+        messages.success(request,("Item Has Been Deleted From The Cart."))
+        return response
